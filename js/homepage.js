713469e45798e8319b09a065ea6baf6ad9964c42
Dael Mondigo <ezradaelmondigo@gmail.com>
2023-02-10 17:24:54 +0800

    upload js file

---
+++ js/homepage.js
@@ -0,0 +1,10 @@
+var login = document.querySelector('#login');
+
+
+login.addEventListener('click', function(){
+	var user = document.querySelector('#user-name').value;
+	var pass = document.querySelector('#pass-word').value;
+	
+	//render data and pass to server with node.js
+	console.log("user: " + user + " \npass: " + pass);
+});
