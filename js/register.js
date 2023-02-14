4ad3c70d97939dfbd37b8c9305fe171c7972bb23
Dael Mondigo <ezradaelmondigo@gmail.com>
2023-02-15 03:08:27 +0800

    Feb. 15, 2023: upload onboarding page where in newly created admin accounts that is not assigned yet to a cemetery will be directed through this page

---
+++ js/register.js
@@ -0,0 +1,22 @@
+var register = document.querySelector("#register");
+
+register.addEventListener('click', function(){
+    var fname = document.querySelector("#fname").value;
+    var lname = document.querySelector("#lname").value;
+    var mi = document.querySelector("#mi").value;
+    var email = document.querySelector("#email").value;
+    var pass = document.querySelector("#pass").value;
+    var re_pass = document.querySelector("#re-pass").value;
+    var terms = document.querySelector("#terms").value;
+    /*
+    console.log("fname: " + fname);
+    console.log("lname: " + lname);
+    console.log("mi: " + mi);
+    console.log("email: " + email);
+    console.log("pass: " + pass);
+    console.log("re-pass: " + re_pass);
+    console.log("terms: " + terms);*/
+
+
+
+});
