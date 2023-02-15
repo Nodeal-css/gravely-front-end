bedd3e181cee2568b3246278da13817dc1563801
McLOVIN <shortcut7777@gmail.com>
2023-02-15 16:31:58 +0800

    feat: add pb collections

--- js/lib.js
+++ js/lib.js
@@ -1,18 +1,39 @@
 const pb = new PocketBase('http://localhost:8090');
 
+function isLoggedIn() {
+  return pb.authStore?.isValid
+}
+
+function getSessionAdmin() {
+  return pb.authStore?.model
+}
+
 function signin(user) {
   return pb.collection('admin').authWithPassword(
       user.username,
       user.password
-  )
+  );
 }
 
 function signout() {
-  pb.authStore.clear();
+  pb.authStore?.clear();
+}
+
+function register(user) {
+  return pb.collection('admin').create(user);
 }
 
 function findMyCemetery() {
-  return pb.collection('admin').getOne(pb.authStore.model.id, {
+  return pb.collection('admin').getOne(pb.authStore?.model.id, {
     expand: 'cemetery_id'
   });
+}
+
+function createCemetery(cemetery) {
+  return pb.collection('cemetery').create(cemetery);
+}
+
+function updateCemetery(cemetery) {
+  const { id, ...data } = cemetery 
+  return pb.collection('cemetery').update(cemetery.id, data);
 }
