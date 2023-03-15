f0122226e59ef0e7899b468ff1bcef9df3725510
Dael Mondigo <ezradaelmondigo@gmail.com>
2023-03-15 19:36:06 +0800

    implemented form validation for registration (admin, cemetery, subscription)

--- js/register.js
+++ js/register.js
@@ -11,13 +11,27 @@ const admin_container = document.querySelector("#admin-container");
 const cemetery_container = document.querySelector("#cemetery-container");
 const subscribe_container = document.querySelector("#subscribe-container");
 
+const id_names = {
+    "fname": "first name",
+    "lname": "last name",
+    "mi": "Middle initial",
+    "email": "Email",
+    "pass": "password",
+    "re-pass": "re-entered password",
+    "cem-name": "cemetery name",
+    "cem-address": "cemetery address",
+    "telephone-1": "telephone line 1",
+    "telephone-2": "telephone line 2",
+    "contact": "contact"
+}
+
 
 admin_reg.addEventListener('click', function(){
     //form validation for admin
     const admin_id_names = ["email", "pass", "re-pass", "fname", "lname", "mi"];
     for(let i = 0; i < admin_id_names.length; i++){
         if(!input_validation(admin_id_names[i])){
-            alert("Invalid input: Please fill up all the forms correctly.");
+            alert("Please Enter a valid " + id_names[admin_id_names[i]] + ".");
             return;
         }
     }
@@ -26,12 +40,8 @@ admin_reg.addEventListener('click', function(){
         return;
     }
 
-    admin_container.style.display = 'none';
-    cemetery_container.style.display = 'block';
-    subscribe_container.style.display = 'none';
-    admin_panel.classList.remove("active");
-    cemetery_panel.classList.add("active");
-    subscribe_panel.classList.remove("active");
+    showCemetery();
+    checkEmailExist(document.querySelector("#email").value);
 });
 
 cemetery_reg.addEventListener('click', function(){
@@ -39,17 +49,11 @@ cemetery_reg.addEventListener('click', function(){
     const cemetery_id_names = ["cem-name", "cem-address", "telephone-1", "telephone-2", "contact"];
     for(let i = 0; i < cemetery_id_names.length; i++){
         if(!input_validation(cemetery_id_names[i])){
-            alert("Invalid input: Please fill up all the forms correctly.");
+            alert("Please enter a valid " + id_names[cemetery_id_names[i]] + ".");
             return;
         }
     }
-    cemetery_container.style.display = 'none';
-    admin_container.style.display = 'none';
-    subscribe_container.style.display = 'block';
-    cemetery_panel.classList.remove("active");
-    subscribe_panel.classList.add("active");
-    admin_panel.classList.remove("active");
-    document.querySelector('#cemetery-name').value = document.querySelector('#cem-name').value;
+    showSubscription();
 });
 
 btn_gcash.addEventListener('click', function(){
@@ -102,14 +106,53 @@ reg.addEventListener('click', function(){
 function input_validation(id){
     let input = document.getElementById(id);
     let flag = false;
-    if(input.checkValidity() && input.value != ""){
+    if(input.checkValidity()){
         flag = true;
     }
     return flag;
 }
 
+//search email if it already exists
+function checkEmailExist(input){
+    search("admin", 1, 100, { email: input }, "+created,email", "cemetery_id")
+    .then( function(data){
+        if(data.items.length > 0 && input != ""){
+            alert("Email already existed, please try a new one.");
+            document.querySelector("#email").value = "";
+            showAdmin();
+        }
+    }).catch( function(err){
+        console.log(err.message);
+    })
+}
 
+function showAdmin(){
+    admin_container.style.display = 'block';
+    cemetery_container.style.display = 'none';
+    subscribe_container.style.display = 'none';
+    admin_panel.classList.add("active");
+    cemetery_panel.classList.remove("active");
+    subscribe_panel.classList.remove("active");
+}
 
+function showCemetery(){
+    admin_container.style.display = 'none';
+    cemetery_container.style.display = 'block';
+    subscribe_container.style.display = 'none';
+    admin_panel.classList.remove("active");
+    cemetery_panel.classList.add("active");
+    subscribe_panel.classList.remove("active");
+}
+
+function showSubscription(){
+    cemetery_container.style.display = 'none';
+    admin_container.style.display = 'none';
+    subscribe_container.style.display = 'block';
+    cemetery_panel.classList.remove("active");
+    subscribe_panel.classList.add("active");
+    admin_panel.classList.remove("active");
+    document.querySelector('#cemetery-name').value = document.querySelector('#cem-name').value;
+}
 /*
     Assignment:
     - Create a form validation for registering admin, cemetery and subscription
