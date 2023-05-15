c9f851831d4d840353eabfcf46c97730ce0b82c8
Dael Mondigo <ezradaelmondigo@gmail.com>
2023-05-15 12:30:00 +0800

    revise: change block designs and add sections

--- public/js/updateCoordinates.js
+++ public/js/updateCoordinates.js
@@ -42,6 +42,7 @@ function pointCoordinates(){
 
 L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=8QM9cnYU5pgNqcMDeMwN', {
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+        maxZoom: 23,
 }).addTo(mapOrig);
 
 mapOrig.addEventListener('mousemove', function(e){
